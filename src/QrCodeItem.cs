4203f31cf9f34c12614423510097690e630058fc
Prasad-Khandekar <KPrasadKhan@yahoo.com>
2026-06-01 11:20:58 +0530

    Create QrCodeItem.cs

---
+++ src/QrCodeItem.cs
@@ -0,0 +1,10 @@
+
+namespace com.freebiz
+{
+    internal class QrCodeItem
+    {
+        public byte[]? Data { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
